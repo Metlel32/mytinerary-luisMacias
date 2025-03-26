4710fb33b842d1cdfeb534f7976e4f471712c7ab
Luis Macias <metlel32@gmail.com>
2025-03-26 11:06:32 -0500

    arreglo detalles y modificaciones de imagenes y estilops  de cityDetail

--- src/components/CityDetails.jsx
+++ src/components/CityDetails.jsx
@@ -15,7 +15,7 @@ function CityDetail() {
                 setCity(data.response)
             } catch (error) {
                 console.error("Error fetching data", error)
-            }finally{
+            } finally {
                 setLoading(false)
             }
         }
@@ -29,12 +29,26 @@ function CityDetail() {
 
     return (
         <div className="flex flex-col items-center bg-indigo-100 justify-center   p-5">
-            <img className="object-cover rounded-2xl" src={city.images} alt={city.name} />
-            <h1 className="text-4xl  mt-5 font-bold mb-4">{city.name}</h1>
-            <button onClick={() => navigate("/cities")} className="mb-5 px-4 py-2 bg-gray-900 hover:bg-gray-700 text-white rounded-lg "> Back to Cities</button>
+
+            <div className="lg:flex lg:flex-row m-5 p-5">
+                <div className="flex flex-col items-center justify-center">
+
+                    <img className="object-cover w-150 rounded-2xl" src={city.images} alt={city.name} />
+
+                </div>
+                <div className="flex lg:mt-5 flex-col items-center justify-center">
+                    <h1 className="text-4xl text-center mt-5 font-bold mb-4">{city.name}</h1>
+                    <p className="p-5 w-200" >{city.description}</p>
+                    <button onClick={() => navigate("/cities")} className="mb-5 px-4 py-2 bg-gray-900 hover:bg-gray-700 text-white rounded-lg "> Back to Cities</button>
+
+                </div>
+            </div>
+
+
+
 
             <div className="bg-orange-500 h-[40vh] w-[99vw] flex items-center justify-center">
-                <p className="text-2xl font-bold text-center pt-5 text-white  ">Under construction ... </p>
+                <img className="w-50 rounded-2xl border" src="../src/images/construction2.jpg" alt="contruction" />
             </div>
 
         </div>
